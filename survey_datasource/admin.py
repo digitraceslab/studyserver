819116822d1a8701ba7e40f8c6928e1dc7213c68
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-06-04 08:57:38 +0300

    Add survey data source admin

--- survey_datasource/admin.py
+++ survey_datasource/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from survey_datasource.models import SurveyDataSource
+
 # Register your models here.
+@admin.register(SurveyDataSource)
+class SurveyDataSourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'device_id', 'profile')
+    search_fields = ['name', 'device_id', 'profile__user__username']
+
--- survey_datasource/apps.py
+++ survey_datasource/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SurveyDatasourceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'survey_datasource'
+
+    def ready(self):
+        from . import forms
